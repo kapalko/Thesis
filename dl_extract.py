7b3ada74163a9d7351e2c2bd0658f3c4a104c6b8
Kyle Palko <mrgtreeskier@users.noreply.github.com>
2015-10-07 23:21:23 -0400

    Update dl_extract.py
    Working on reading through each file in the folder and applying the mask and then extracting the time series data from it.

--- dl_extract.py
+++ dl_extract.py
@@ -2,20 +2,21 @@
 This python script is used to automatically download the selected data to a machine, extract it, and then move it to a
 working directory.
 
-Date: 29 September 2015
+Date: 07 October 2015
 """
 
 __author__ = '2d Lt Kyle Palko'
-__version__ = 'v0.3.1'
-
+__version__ = 'v0.3.2'
+from datetime import datetime
 from nilearn import datasets
 import glob
 import os
 import gzip
 
+start_time=datetime.now()
 path = '/media/kap/8e22f6f8-c4df-4d97-a388-0adcae3ec1fb/Python/Thesis/Test'  # set where the data should be saved
-sub_id = [50004, 50121, 50003]  # sets the subject ids that should be pulled simple 3
-# sub_id = range(50003, 50060)  # calls all of the PITT study subjects
+# sub_id = [50060]  # sets the subject ids that should be pulled simple 3
+# sub_id = range(50003, 50061)  # calls all of the PITT study subjects
 pipeline = 'cpac'  # define the pipeline used to preprocess the data
 derivative = 'func_preproc'  # define what data should be pulled
 
@@ -48,3 +49,15 @@ for n in sub_id:
         os.remove(name)  # deletes the .nii.gz file
 # at this point, I would just move to the folder, select all, and move to the data folder that you desire to house
 # the data
+print datetime.now() - start_time
+
+from nilearn.masking import apply_mask
+from nilearn.input_data import NiftiLabelsMasker
+
+masker = NiftiLabelsMasker(labels_img='/media/kap/8e22f6f8-c4df-4d97-a388-0adcae3ec1fb/Python/Thesis/TT/tt_mask_pad.nii'
+                           , standardize=True)  # sets the atlas used
+
+for n in glob.glob('*[0-9].nii'):
+    str_id = n[:5]  # sets the current image ID
+    masked_data = apply_mask(n, str_id+'_mask.nii')
+    time_series = masker.fit_transform(my_data[0])
