cf2e5f529ba13a526efe6f3898e4250a35c74914
Kyle Palko <mrgtreeskier@users.noreply.github.com>
2015-10-19 18:36:09 -0400

    Update corr_run.py

--- corr_run.py
+++ corr_run.py
@@ -4,7 +4,7 @@ Run this when you already have your data downloaded and unzipped.
 Date: 19 October 2015
 """
 __author__ = '2d Lt Kyle Palko'
---version__ = 'v0.1.0'
+__version__ = 'v0.1.0'
 
 import glob
 import os
@@ -15,8 +15,8 @@ from nilearn.input_data import NiftiLabelsMasker
 masker = NiftiLabelsMasker(labels_img='/media/kap/8e22f6f8-c4df-4d97-a388-0adcae3ec1fb/Python/Thesis/TT/tt_mask_pad.nii'
                            , standardize=True)  # sets the atlas used
 atlas = 'TT'  # label which atlas to use
-stud = 'Pitt'
-os.chdir('/media/kap/8e22f6f8-c4df-4d97-a388-0adcae3ec1fb/Python/Thesis/Data/Pitt')
+stud = 'Olin'
+os.chdir('/media/kap/8e22f6f8-c4df-4d97-a388-0adcae3ec1fb/Python/Thesis/Data/{0}'.format(stud))
 for n in sorted(glob.glob('*[0-9].nii')):
     str_id = n[:5]  # sets the current image ID
 #    masked_data = apply_mask(n, str_id+'_mask.nii')
