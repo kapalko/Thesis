d6d58108275fbe2ade5eb01abdcba26ef5f2d7db
Kyle Palko <mrgtreeskier@users.noreply.github.com>
2015-10-19 17:02:21 -0400

    Update dl_extract.py

--- dl_extract.py
+++ dl_extract.py
@@ -62,17 +62,17 @@ stud = 'Pitt'
 os.chdir('/media/kap/8e22f6f8-c4df-4d97-a388-0adcae3ec1fb/Python/Thesis/Data/Pitt')
 for n in sorted(glob.glob('*[0-9].nii')):
     str_id = n[:5]  # sets the current image ID
-#    masked_data = apply_mask(n, str_id+'_mask.nii')    
+#    masked_data = apply_mask(n, str_id+'_mask.nii')
     ts = masker.fit_transform('{0}.nii'.format(str_id))
-    
-    norm = np.corrcoeff(ts.t)
-    
+
+    norm = np.corrcoef(ts.T)
+
     # flatten the correlation matrix
     cors = [str_id]
     for i in range(1, np.size(norm, axis=0)):
-        for j in range (i+1, np.size(norm, axis=0)):
-            cors.append(norm[i,j])
-     
+        for j in range(i+1, np.size(norm, axis=0)):
+            cors.append(norm[i, j])
+
     # write the correlations to a CSV
     with open('{1}_{0}.csv'.format(atlas, stud), 'ab') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',')
