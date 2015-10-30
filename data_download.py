d9c2319f6f12911c7353da68ec5a9c54e4b93ee9
Kyle Palko <mrgtreeskier@users.noreply.github.com>
2015-10-29 22:36:01 -0400

    Update data_download.py
    Downloads all the images from a given pipeline and filtering

--- data_download.py
+++ data_download.py
@@ -1,16 +1,12 @@
-"""
-This file will automatically pull the data from the storage on the cloud. Afterwards, you must extract and put the files
-into your working directory.
-
-See https://nilearn.github.io/modules/generated/nilearn.datasets.fetch_abide_pcp.html#nilearn.datasets.fetch_abide_pcp
-for instructions
-"""
-
-__author__ = 'kap'
+__author__ = '2d Lt Kyle Palko'
 
 from nilearn import datasets
-ab_img_one = datasets.fetch_abide_pcp(n_subjects=2, pipeline='cpac', band_pass_filtering=True, derivatives=['func_preproc'], SUB_ID=[50003])
-ab_img_two = datasets.fetch_abide_pcp(n_subjects=2, pipeline='cpac', band_pass_filtering=True, derivatives=['func_preproc'], SUB_ID=[50004])
+import time
 
-ab_mask_one = datasets.fetch_abide_pcp(n_subjects=2, pipeline='cpac', band_pass_filtering=True, derivatives=['func_mask'], SUB_ID=[50003])
-ab_mask_two = datasets.fetch_abide_pcp(n_subjects=2, pipeline='cpac', band_pass_filtering=True, derivatives=['func_mask'], SUB_ID=[50003])
+start = time.time()
+path = '/media/kap/8e22f6f8-c4df-4d97-a388-0adcae3ec1fb/Python/Thesis/Test'  # set where the data should be saved
+pipeline = 'cpac'  # define the pipeline used to preprocess the data
+derivative = 'func_preproc'  # define what data should be pulled
+stud = '/ABIDE_pcp/cpac/filt_noglobal/e806a9ef657f316b760441d3649f7cb6'
+
+datasets.fetch_abide_pcp(data_dir=path, pipeline=pipeline, band_pass_filtering=True, derivatives=[derivative])
