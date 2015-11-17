eec49a25fdc3deb6e6e8606eff52c6d4117eb5a1
Kyle Palko <mrgtreeskier@users.noreply.github.com>
2015-11-16 20:55:36 -0500

    Update RIP_sim.py
    Begin building functions and simulation

--- RIP_sim.py
+++ RIP_sim.py
@@ -1,11 +1,29 @@
+"""
+This file is used to perform the Reverse Isometry Property simulations in an attempt to find the probability that
+the RIP condition holds for the reduced model.
+
+Date: 15 November 15
+"""
+
 __author__ = '2d Lt Kyle Palko'
-__version__ = 'v0.0.1'
+__version__ = 'v0.0.2'
 
 import numpy as np
 from sklearn.preprocessing import normalize as norm
+import random
 
 # constants
 csv_path = 'tt_cpac_filt_noglobal.csv'
+num_runs = 10
+max_s = 10
+run = True
+b = 1
+
+def delt(beta, vect):
+    # use optimization to find the minimum positive delta with constraints
+    # (1-d)||b||_2^2 <= ||Xb||_2^2 <= (1+d)||b||_2^2
+
+    return d
 
 # get the data
 data = np.genfromtxt(csv_path, delimiter=',')
@@ -15,5 +33,17 @@ Y = np.array([x[1]-1 for x in data])  # y values in the second column
 X = np.array([x[2:] for x in data])
 del x
 del data
-
 x_norm = norm(X, axis=0)
+n_feat = np.size(X, axis=1)
+
+results = np.zeros(num_runs, max_s)
+
+while run and b <= max_s:
+    print b
+
+    for i in range(0, num_runs):
+        beta = np.repeat(1/np.sqrt(b), b)  # creates vector of betas
+        z = np.random.random_integers(0, n_feat-1, b)  # gets random integers for columns from data
+        vect = x_norm[:, z]  # stores normal x values in a new array to multiply by betas
+        results[i, b] = delt(beta, vect)
+
