f7a6beac7063571e8426808775cfc8871e8cc5d5
Kyle Palko <mrgtreeskier@users.noreply.github.com>
2015-11-23 23:44:57 -0500

    sorts the data before running

--- experiment_test.py
+++ experiment_test.py
@@ -5,7 +5,7 @@ Date: 23 November 15
 """
 
 __author__ = '2d Lt Kyle Palko'
-__version__ = 'v0.0.4'
+__version__ = 'v0.0.5'
 
 from random import shuffle
 from random import seed
@@ -67,8 +67,10 @@ class BeginClass():
 
 # get the data
 data = np.genfromtxt(d_path, delimiter=',')
+
 # remove rows that have NaN values (not ideal but IDGAF yet)
 data = data[~np.isnan(data).any(axis=1)]  # from stack overflow: https://bit.ly/1QhfcmZ
+data = sorted(data, key=lambda x: x[0])
 Y = np.array([x[1]-1 for x in data])  # y values in the second column
 X = np.array([x[2:] for x in data])
 del x
