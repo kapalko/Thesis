c64e90398b9aec0334d0fd2c0c6304c80cd5c1a4
mrgtreeskier <mrgtreeskier@gmail.com>
2015-11-25 19:43:35 -0500

    trimmed fat, added more generality

--- 1D_files.py
+++ 1D_files.py
@@ -14,21 +14,22 @@ from nilearn import datasets
 __author__ = '2d Lt Kyle Palko'
 __version__ = 'v0.1'
 
-path = '/media/kap/8e22f6f8-c4df-4d97-a388-0adcae3ec1fb/Python/Thesis/Test'  # set where the data should be saved
-pipeline = 'cpac'  # define the pipeline used to preprocess the data
-derivative = 'rois_tt'  # define what data should be pulled
+# path = '/media/kap/8e22f6f8-c4df-4d97-a388-0adcae3ec1fb/Python/Thesis/Test'  # set where the data should be saved
+path = '/home/kap/Thesis/Data/DL'
+pipe = 'cpac'  # define the pipeline used to preprocess the data
+derivative = 'rois_dosenbach160'  # define what data should be pulled
 
-datasets.fetch_abide_pcp(data_dir=path, pipeline=pipeline, band_pass_filtering=True, global_signal_regression=False,
+datasets.fetch_abide_pcp(data_dir=path, pipeline=pipe, band_pass_filtering=True, global_signal_regression=False,
                          derivatives=[derivative])
 
 # local variables and paths #
-path = '/media/kap/8e22f6f8-c4df-4d97-a388-0adcae3ec1fb/Python/Thesis/'  # working directory
-pipe = 'cpac'
+# path = '/media/kap/8e22f6f8-c4df-4d97-a388-0adcae3ec1fb/Python/Thesis/'  # working directory
 filt = 'filt_noglobal'
-stud = 'Test/ABIDE_pcp/{0}/{1}/'.format(pipe, filt)  # location that download happened
+# stud = 'Test/ABIDE_pcp/{0}/{1}/'.format(pipe, filt)  # location that download happened
+stud = 'ABIDE_pcp/{0}/{1}/'.format(pipe, filt)
 # stud = 'Data/'
-lab = path + 'Data/'  # location of CSV files for labeling
-mask_name = 'TT_prep'
+lab = path + 'Label/'  # location of CSV files for labeling
+mask_name = 'dos160_prep'
 
 # build two lists of strings from CSV files to use to match the subjects and their diagnosis
 idlab = []  # subject IDs
