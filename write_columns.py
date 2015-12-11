c0e38ef2451a6ed770669e30baa66534a9872787
mrgtreeskier <mrgtreeskier@gmail.com>
2015-12-10 23:24:02 -0500

    fixed bug

--- write_columns.py
+++ write_columns.py
@@ -8,16 +8,16 @@ __author__ = '2d Lt Kyle Palko'
 import csv
 import numpy as np
 
-num_roi = 96  # number of ROIs in the atlas
+num_roi = 200  # number of ROIs in the atlas
 col_one = []
 col_two = []
-for i in range(1, 96):
-    for j in range(i+1, 97):
+for i in range(1, num_roi):
+    for j in range(i+1, num_roi+1):
         col_one.append(i)
         col_two.append(j)
 
 for i in range(0, np.size(col_one, axis=0)):
-    with open('columns.csv', 'ab') as csvfile:
+    with open('cc200_columns.csv', 'ab') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',')
         spamwriter.writerow((col_one[i], col_two[i]))
     csvfile.close()
